3daff7029d4ee4ed33f27d3d295dccd5953431f5
Hbr8792 <Hbr8792@stacmail.net>
2022-03-03 09:10:18 +1300

    Create Raffle.py

---
+++ Raffle.py
@@ -0,0 +1,7 @@
+Print("Kia ora, Welcome to the St Andrews College Raffle program")
+Ask_da_Person = true
+while Ask_da_Person == true
+    prize = int()
+    if prize == "stop
+        Ask_da_Person = false
+    else: print("The prize for this raffle is (prize)
