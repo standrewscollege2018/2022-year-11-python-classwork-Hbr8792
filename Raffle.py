9dab1c825d23abe14d40d4423df9b03d361d0eaa
Hbr8792 <Hbr8792@stacmail.net>
2022-03-04 11:35:13 +1300

    Update Raffle.py

--- Raffle.py
+++ Raffle.py
@@ -1,4 +1,5 @@
 #This Program is for the St Andrews College raffle
+names = []
 print("Kia ora, Welcome to the St Andrews College Raffle program")
 ask_da_Person = True
 while ask_da_Person == True:
@@ -13,6 +14,19 @@ while ask_da_Person == True:
 #Selecting the prize
         
 get_value = True
+value = input("Please enter prize value")
+value = float(value)
+
+ask_name = True
+while ask_name == True:
+    name = input("Plese enter name")
+    if name == "end":
+        ask_name = False
+    else:
+        names.append(name)
+
+print(names)
+
 
 
 
