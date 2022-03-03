61585d7d406642eaf4d6ab859cc5a79605a526f3
Hbr8792 <Hbr8792@stacmail.net>
2022-03-04 11:11:07 +1300

    Update Raffle.py

--- Raffle.py
+++ Raffle.py
@@ -1,7 +1,20 @@
-Print("Kia ora, Welcome to the St Andrews College Raffle program")
-Ask_da_Person = true
-while Ask_da_Person == true
-    prize = int()
-    if prize == "stop
-        Ask_da_Person = false
-    else: print("The prize for this raffle is (prize)
+#This Program is for the St Andrews College raffle
+print("Kia ora, Welcome to the St Andrews College Raffle program")
+ask_da_Person = True
+while ask_da_Person == True:
+    prize = input("Enter the prize being raffled: ")
+    if prize == "":
+        print("Please enter a valid prize.")
+    else:
+        print(f"The prize for this raffle is {prize}.")
+        ask_da_Person = False
+
+
+#Selecting the prize
+        
+get_value = True
+
+
+
+                
+                
