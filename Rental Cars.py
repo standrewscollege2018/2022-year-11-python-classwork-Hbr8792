ad9451e540fcc65eb285fd439c7c0e06bef2c137
Hbr8792 <Hbr8792@stacmail.net>
2022-03-07 12:35:32 +1300

    Create Rental Cars.py

---
+++ Rental Cars.py
@@ -0,0 +1,14 @@
+print("Welcome to the University vehicle rental system")
+Car_List = (" Suzuki Van Seats:2,"
+            " Toyota Corolla Seats:4,"
+            " Honda CRV Seats:4,"
+            " Suzuki Swift Seats:4,"
+            " Mitsubishi Airtrek Seats:4,"
+            " Nissan DC Ute Seats:4,"
+            " Toyota Previa Seats:7,"
+            " Toyota Hi Ace Seats:12,"
+            " Toyota Hi Ace Seats:12.")
+print(Car_List)
+
+input("What car would you like to rent?:")
+if input
