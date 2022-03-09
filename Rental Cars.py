949046c1d9bb8ac43dcf3b9eb0e5913d8032b6ba
Hbr8792 <Hbr8792@stacmail.net>
2022-03-10 09:22:35 +1300

    Update Rental Cars.py

--- Rental Cars.py
+++ Rental Cars.py
@@ -1,5 +1,5 @@
 print("Welcome to the University vehicle rental system")
-Car_List = [" 1.Suzuki Van Seats:2",
+car_list = [" 1.Suzuki Van Seats:2",
             " 2.Toyota Corolla Seats:4",
             " 3.Honda CRV Seats:4",
             " 4.Suzuki Swift Seats:4",
@@ -8,15 +8,27 @@ Car_List = [" 1.Suzuki Van Seats:2",
             " 7.Toyota Previa Seats:7",
             " 8.Toyota Hi Ace Seats:12",
             " 9.Toyota Hi Ace Seats:12."]
+avail = ["Available", "Available", "Available", "Available", "Available", "Available", "Available", "Available", "Available"]
+run_program = True
+while run_program == True:
+    for i in range(len(car_list)):
+        print(i+1, car_list[i], avail[i])
+    check = True
+    while check == True:
+        number = int(input("Please enter car number would you like to rent?:"))
+        
+        if number >9 or number <0:
+            print("Please input vaild car number")
+        else:
+            check = False
+    if number == 0:
+        run_program = False
+    else:
+        avail[number-1] = "Unavailable"
+      
+        print(f"{car_list[number-1]}")
 
-print(Car_List)
 
-number = input("Please enter car number would you like to rent?:")
-if number in range (1,9):
-    print("Thank you, the car will be ready shortly")
-else:
-    print("Please input vaild car number")
-if number == ("8"):
-    Print("This car is currently unavaible please enter vaid car number")
+        print("Thank you for using University Vehicle Rental System your car will be ready shortly")
 
 
